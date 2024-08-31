8c8c70b468a303a90c1b16db41469145ff6ff52e
Alejandra-qm19 <carla.quispe@utec.edu.pe>
2024-08-31 13:59:01 -0500

    Archivo test

---
+++ test1.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from uuid import UUID, uuid4
+from fastapi import FastAPI
+
+
+class Producto(BaseModel):
+    id: Optional[UUID] = None
+    nombre: str
+    descripcion: Optional[str] = None
+    precio: float
+    stock: int
+    
+app = FastAPI()
